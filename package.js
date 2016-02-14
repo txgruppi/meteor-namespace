cdb18de1c0c6c23bd93c83a241ece30d837c3efd
Tarcísio Gruppi <txgruppi@gmail.com>
2016-02-14 13:36:53 -0200

    Updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "txgruppi:namespace",
-  version: "0.0.1",
+  version: "0.0.2",
   summary: "Namespace helper for Meteor",
   git: "https://github.com/txgruppi/meteor-namespace",
   documentation: "README.md"
